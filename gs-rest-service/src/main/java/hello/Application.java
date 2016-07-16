4ec0a9bb64365fabf68f449873da8c8a214716dd
svetlana.chapskaya <svetlana.chapskaya@ab-soft.net>
2016-07-16 17:48:42 +0300

    add Application and GreetingController

---
+++ gs-rest-service/src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Svetlana Chapskaya
+ * Date: 16.07.2016
+ * Time: 17:42
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Svetlana Chapskaya
+ * Date: 16.07.2016
+ * Time: 17:44
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
